f966d88c5987007d9e3f3e09134a36d969a1d739
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2022-11-10 01:12:54 +0530

    modularized

--- Ebay.SoapAPI.Win/Form1.Designer.vb
+++ Ebay.SoapAPI.Win/Form1.Designer.vb
@@ -23,28 +23,66 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnUpdateInventory = New System.Windows.Forms.Button()
+        Me.btnLogin = New System.Windows.Forms.Button()
+        Me.txtToken = New System.Windows.Forms.TextBox()
+        Me.btnGetToken = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnUpdateInventory
         '
-        Me.btnUpdateInventory.Location = New System.Drawing.Point(273, 200)
+        Me.btnUpdateInventory.Location = New System.Drawing.Point(393, 168)
         Me.btnUpdateInventory.Name = "btnUpdateInventory"
         Me.btnUpdateInventory.Size = New System.Drawing.Size(344, 125)
         Me.btnUpdateInventory.TabIndex = 0
-        Me.btnUpdateInventory.Text = "Bulk Update Price / Quantity"
+        Me.btnUpdateInventory.Text = "3. Bulk Update Price / Quantity"
         Me.btnUpdateInventory.UseVisualStyleBackColor = True
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Location = New System.Drawing.Point(12, 12)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(158, 75)
+        Me.btnLogin.TabIndex = 1
+        Me.btnLogin.Text = "1. Login"
+        Me.btnLogin.UseVisualStyleBackColor = True
+        '
+        'txtToken
+        '
+        Me.txtToken.Location = New System.Drawing.Point(13, 108)
+        Me.txtToken.Multiline = True
+        Me.txtToken.Name = "txtToken"
+        Me.txtToken.ReadOnly = True
+        Me.txtToken.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.txtToken.Size = New System.Drawing.Size(347, 330)
+        Me.txtToken.TabIndex = 2
+        '
+        'btnGetToken
+        '
+        Me.btnGetToken.Location = New System.Drawing.Point(177, 13)
+        Me.btnGetToken.Name = "btnGetToken"
+        Me.btnGetToken.Size = New System.Drawing.Size(183, 74)
+        Me.btnGetToken.TabIndex = 3
+        Me.btnGetToken.Text = "2. Get Token"
+        Me.btnGetToken.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnGetToken)
+        Me.Controls.Add(Me.txtToken)
+        Me.Controls.Add(Me.btnLogin)
         Me.Controls.Add(Me.btnUpdateInventory)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnUpdateInventory As Button
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents txtToken As TextBox
+    Friend WithEvents btnGetToken As Button
 End Class
--- Ebay.SoapAPI.Win/Form1.vb
+++ Ebay.SoapAPI.Win/Form1.vb
@@ -2,7 +2,15 @@
 Imports Ebay.Service.Call
 Imports Ebay.Service.Core.Sdk
 
+''' <summary>
+''' Please make sure that you set the proper ApiServerUrl and ApiToken 
+''' in the app.config file BEFORE running this project.
+''' This version does not use the Token value stored in app.config.  Instead it is generated at run time.
+''' </summary>
 Public Class Form1
+
+    Private context As ApiContext
+    Private sid As String
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -10,14 +18,72 @@ Public Class Form1
     End Sub
 
 
-    ''' <summary>
-    ''' Please make sure that you set the proper ApiServerUrl and ApiToken 
-    ''' in the app.config file BEFORE running this project.
-    ''' </summary>
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+        Try
+            Cursor = Cursors.WaitCursor
+            context = GetApiContext()
+            context.ApiCredential.ApiAccount.Application = "please fill this"
+            context.ApiCredential.ApiAccount.Certificate = "please fill this"
+            context.ApiCredential.ApiAccount.Developer = "please fill this"
+            context.ApiCredential.eBayAccount.UserName = "please fill this"
+            context.ApiCredential.eBayAccount.Password = "please fill thisa"
+
+            Dim s = New Ebay.Service.Call.GetSessionIDCall(context)
+
+            Dim runame = "please fill this"
+            sid = s.GetSessionID(runame)
+
+            'this link will open in browser.  Login as seller and complete the process successfully in the browser and then
+            'return to the app and click "Get Token" Button.
+            Dim signinurl = $"https://signin.sandbox.ebay.com/ws/eBayISAPI.dll?SignIn&runame={runame}&SessID={sid}"
+
+            context.RuName = runame
+            context.SignInUrl = signinurl
+
+            Dim si = New ProcessStartInfo(signinurl)
+            si.UseShellExecute = True
+            System.Diagnostics.Process.Start(si)
+
+            Cursor = Cursors.Default
+            MessageBox.Show("Login Successfull!", "SUCCESS", MessageBoxButtons.OK, MessageBoxIcon.Asterisk)
+
+        Catch ex As Exception
+            Cursor = Cursors.Default
+            Dim errorMessage = "Error while logging in:" + Environment.NewLine + Environment.NewLine
+            errorMessage += ex.Message + Environment.NewLine
+            errorMessage += ex.InnerException?.Message
+
+            MessageBox.Show(errorMessage, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+
+
+    End Sub
+
+    Private Sub btnGetToken_Click(sender As Object, e As EventArgs) Handles btnGetToken.Click
+        Try
+            Cursor = Cursors.WaitCursor
+            Dim au = New Ebay.Service.Call.FetchTokenCall(context)
+            context.ApiCredential.eBayToken = au.FetchToken(sid)
+            txtToken.Text = context.ApiCredential.eBayToken
+            Cursor = Cursors.Default
+            'Once token is received you can call the any api end points.
+        Catch ex As Exception
+            Cursor = Cursors.Default
+            Dim errorMessage = "Error while Fetching Token:" + Environment.NewLine + Environment.NewLine
+            errorMessage += ex.Message + Environment.NewLine
+            errorMessage += ex.InnerException?.Message
+
+            MessageBox.Show(errorMessage, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        End Try
+    End Sub
+
+
     Private Sub btnUpdateInventory_Click(sender As Object, e As EventArgs) Handles btnUpdateInventory.Click
         Try
-            Dim context = GetApiContext()
             Dim apiCall = New ReviseInventoryStatusCall(context)
+
 
             Dim InventoriesToUpdate = GetInventoriesToUpdateList()
             Dim updatedInventory = apiCall.ReviseInventoryStatus(InventoriesToUpdate)
